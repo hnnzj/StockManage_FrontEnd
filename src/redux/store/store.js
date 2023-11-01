dc33c4a05e4a08d555be337069830b79fab787ea
hnnzj <heinnitzj@gmail.com>
2023-10-31 23:58:19 -0300

    Ready to deploy

---
+++ src/redux/store/store.js
@@ -0,0 +1,17 @@
+import { configureStorex } from "@reduxjs/toolkit";
+import { productSlice } from "./slice/productSlice";
+import { clientSlice } from "./slice/clientSlice";
+import { pedidosSlice } from "./slice/pedidosSlice";
+
+export const store = configureStore({
+  reducer: {
+    products: productSlice.reducer,
+    clients: clientSlice.reducer,
+    pedidos: pedidosSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: false,
+});
